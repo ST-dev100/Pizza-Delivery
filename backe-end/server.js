687d47436587514a1ae32ab87bb36408d8112905
simontamene.dev@gmail.com <simontamene.dev@gmail.com>
2024-09-30 13:24:00 +0300

    m

---
+++ backe-end/server.js
@@ -0,0 +1,13 @@
+const express = require("express")
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.json({a:"bbb"})
+})
+app.listen(3000,()=>{
+    console.log("hello")
+})
+
+// Export the serverless function
+module.exports = app;
